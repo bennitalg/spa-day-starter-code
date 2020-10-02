18e77423252566fb7e66ee78144ffc41b059539f
bennitalg <40967401+bennitalg@users.noreply.github.com>
2020-10-02 16:56:38 -0500

    Still pushing

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,20 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
